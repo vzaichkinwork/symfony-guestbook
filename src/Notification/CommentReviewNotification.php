70734322f278811ac117b593e42113ebd22021a2
VitaliiZaichkin <vitalii.zaichkin@introduct.tech>
2022-04-28 10:38:57 +0300

    adds telegram admin comment approval

--- src/Notification/CommentReviewNotification.php
+++ src/Notification/CommentReviewNotification.php
@@ -3,19 +3,26 @@
 namespace App\Notification;
 
 use App\Entity\Comment;
+use Symfony\Component\Notifier\Bridge\Telegram\Reply\Markup\Button\InlineKeyboardButton;
+use Symfony\Component\Notifier\Bridge\Telegram\Reply\Markup\InlineKeyboardMarkup;
+use Symfony\Component\Notifier\Bridge\Telegram\TelegramOptions;
+use Symfony\Component\Notifier\Message\ChatMessage;
 use Symfony\Component\Notifier\Message\EmailMessage;
+use Symfony\Component\Notifier\Notification\ChatNotificationInterface;
 use Symfony\Component\Notifier\Notification\EmailNotificationInterface;
 use Symfony\Component\Notifier\Notification\Notification;
 use Symfony\Component\Notifier\Recipient\EmailRecipientInterface;
 use Symfony\Component\Notifier\Recipient\RecipientInterface;
 
-class CommentReviewNotification extends Notification implements EmailNotificationInterface
+class CommentReviewNotification extends Notification implements EmailNotificationInterface, ChatNotificationInterface
 {
     private $comment;
+    private $reviewUrl;
 
-    public function __construct(Comment $comment)
+    public function __construct(Comment $comment, string $reviewUrl)
     {
         $this->comment = $comment;
+        $this->reviewUrl = $reviewUrl;
 
         parent::__construct('New comment posted');
     }
@@ -26,6 +33,34 @@ class CommentReviewNotification extends Notification implements EmailNotificatio
         $message->getMessage()
             ->htmlTemplate('emails/comment_notification.html.twig')
             ->context(['comment' => $this->comment]);
+
+        return $message;
+    }
+
+    public function asChatMessage(RecipientInterface $recipient, string $transport = null): ?ChatMessage
+    {
+        if ('telegram' !== $transport) {
+            return null;
+        }
+
+        $message = ChatMessage::fromNotification($this, $recipient, $transport);
+        $message->subject($this->getSubject());
+
+        $telegramOptions = (new TelegramOptions())
+//            ->chatId('171311304')
+            ->parseMode('MarkdownV2')
+            ->disableWebPagePreview(true)
+            ->disableNotification(true)
+            ->replyMarkup((new InlineKeyboardMarkup())
+                ->inlineKeyboard([
+                    (new InlineKeyboardButton('Accept'))
+                        ->url($this->reviewUrl),
+                    (new InlineKeyboardButton('Reject'))
+                        ->url($this->reviewUrl . '?reject=1'),
+                ])
+            );
+
+        $message->options($telegramOptions);
 
         return $message;
     }
